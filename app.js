bc00823bd67e2c3cf3c3687e9d72eab153618359
Kiryl Korzun <megaflex2012@gmail.com>
2016-04-27 08:39:47 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var routes = require('./routes/index');
+
+var app = express();
+
+app.set('views', __dirname + '/template');
+app.set('view engine', 'ejs');
+
+app.use('/', routes);
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {body: '<b>404: Not Found</b>',
+        error: err.status, message: err.message
+    });
+});
+
+module.exports = app;
